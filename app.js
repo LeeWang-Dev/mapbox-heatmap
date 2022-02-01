f619a9c99899f7419b5673a83a71249f6e76aed6
Alessandra-Corallo <52160214+LeeWang-Dev@users.noreply.github.com>
2022-02-01 17:10:37 +0100

    fix changes

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, 'client', 'index.html'));
+   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
